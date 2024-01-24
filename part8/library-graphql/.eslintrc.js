238d158175fd20335b34c2156446e354f3f4de15
russelsy <russelgabrielsy@gmail.com>
2024-01-24 14:24:34 -0500

    finished 8.7

---
+++ part8/library-graphql/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+		"no-unused-vars": "off",
+	}
+}
---
+++ part8/library-graphql/index.js
@@ -0,0 +1,203 @@
+const { ApolloServer } = require("@apollo/server")
+const { startStandaloneServer } = require("@apollo/server/standalone")
+const { v1: uuid } = require("uuid")
+const { GraphQLError } = require("graphql")
+
+let authors = [
+	{
+		name: "Robert Martin",
+		id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+		born: 1952,
+	},
+	{
+		name: "Martin Fowler",
+		id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+		born: 1963
+	},
+	{
+		name: "Fyodor Dostoevsky",
+		id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+		born: 1821
+	},
+	{ 
+		name: "Joshua Kerievsky", // birthyear not known
+		id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+	},
+	{ 
+		name: "Sandi Metz", // birthyear not known
+		id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+	},
+]
+
+/*
+ * English:
+ * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
+ * However, for simplicity, we will store the author's name in connection with the book
+*/
+
+let books = [
+	{
+		title: "Clean Code",
+		published: 2008,
+		author: "Robert Martin",
+		id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+		genres: ["refactoring"]
+	},
+	{
+		title: "Agile software development",
+		published: 2002,
+		author: "Robert Martin",
+		id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+		genres: ["agile", "patterns", "design"]
+	},
+	{
+		title: "Refactoring, edition 2",
+		published: 2018,
+		author: "Martin Fowler",
+		id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+		genres: ["refactoring"]
+	},
+	{
+		title: "Refactoring to patterns",
+		published: 2008,
+		author: "Joshua Kerievsky",
+		id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+		genres: ["refactoring", "patterns"]
+	},  
+	{
+		title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+		published: 2012,
+		author: "Sandi Metz",
+		id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+		genres: ["refactoring", "design"]
+	},
+	{
+		title: "Crime and punishment",
+		published: 1866,
+		author: "Fyodor Dostoevsky",
+		id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+		genres: ["classic", "crime"]
+	},
+	{
+		title: "The Demon ",
+		published: 1872,
+		author: "Fyodor Dostoevsky",
+		id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+		genres: ["classic", "revolution"]
+	},
+]
+
+/*
+  you can remove the placeholder query once your first one has been implemented 
+*/
+
+const typeDefs = `
+	type Author {
+		name: String!
+		id: ID!
+		born: Int
+		bookCount: Int!
+	}
+
+	type Book {
+		title: String!
+		published: Int!
+		author: String!
+		id: ID!
+		genres: [String!]!
+	}
+
+  type Query {
+    bookCount: Int!
+		authorCount: Int!
+		allBooks(author: String, genre: String): [Book!]
+		allAuthors: [Author!]!
+  }
+
+	type Mutation {
+		addBook(
+			title: String!
+			author: String!
+			published: Int!
+			genres: [String!]!
+		): Book
+		editAuthor(
+			name: String!
+			setBornTo: Int!
+		): Author
+	}
+`
+
+const resolvers = {
+	Query: {
+		bookCount: () => books.length,
+		authorCount: () => authors.length,
+		allBooks: (root, args) => {
+			if (!args.author && !args.genre) {
+				return books
+			}
+			let retBooks = books
+			if (args.author) {
+				retBooks = retBooks.filter(book => book.author === args.author)
+			}
+			if (args.genre) {
+				retBooks = retBooks.filter(book => book.genres.includes(args.genre))
+			}
+			return retBooks
+		},
+		allAuthors: () => authors,
+	},
+	Mutation: {
+		addBook: (root, args) => {
+			if (books.find(book => book.title === args.title)) {
+				throw new GraphQLError("Book title must be unique", {
+					extensions: {
+						code: "BAD_USER_INPUT",
+						invalidArgs: args.title
+					}
+				})
+			}
+
+			if (!authors.find(author => author.name === args.author)) {
+				const author = { name: args.author, id: uuid() }
+				authors = authors.concat(author)
+			}
+		
+			const book = { ...args, id: uuid() }
+			books = books.concat(book)
+
+			return book
+		},
+		editAuthor: (root, args) => {
+			const author = authors.find(author => author.name === args.name)
+			if (!author) {
+				throw new GraphQLError("Author not found", {
+					extensions: {
+						code: "BAD_USER_INPUT",
+						invalidArgs: args.name
+					}
+				})
+			}
+
+			const editedAuthor = {...author, born: args.setBornTo}
+			authors = authors.map(author => author.name === args.name ? editedAuthor : author)
+			return editedAuthor
+		}
+	},
+	Author: {
+		bookCount: (root, args) => {
+			return books.filter(book => book.author === root.name).length
+		}
+	}
+}
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+})
+
+startStandaloneServer(server, {
+	listen: { port: 4000 },
+}).then(({ url }) => {
+	console.log(`Server ready at ${url}`)
+})
