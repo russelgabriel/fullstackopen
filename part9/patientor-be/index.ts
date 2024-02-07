6f3b03981dc2a20c963952e7a064c5147661bbe1
russelsy <russelgabrielsy@gmail.com>
2024-02-07 11:28:07 -0500

    finished 9.9

---
+++ part9/patientor-be/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3001;
+
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
